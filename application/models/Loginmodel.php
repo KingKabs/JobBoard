a340e1e4d668a151bc05feab9e3422d5245fa925
PK <pwkabunyi@gmail.com>
2023-06-17 15:33:27 +0300

    updated login

--- application/models/Loginmodel.php
+++ application/models/Loginmodel.php
@@ -10,13 +10,17 @@ class Loginmodel extends CI_Model {
         $this->db->select('id, username, password, login_type');
         $this->db->from('auth_users');
         $this->db->where('username', $username);
-        $this->db->where('password', MD5($password));
         $this->db->limit(1);
 
         $query = $this->db->get();
 
         if ($query->num_rows() == 1) {
-            return $query->result();
+            $user = $query->row();
+            if (password_verify($password, $user->password)) {
+                return $query->result();
+            } else {
+                return false;
+            }
         } else {
             return false;
         }
